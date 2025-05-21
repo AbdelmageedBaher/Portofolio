5a8458e03f81de34782bd54dcfb757096fecbe23
eng.Abdelmageed <abdelmageedbaher12@gmail.com>
2025-05-21 17:24:06 +0300

    add project

--- src/app/component/Projects.jsx
+++ src/app/component/Projects.jsx
@@ -7,6 +7,14 @@ import ProjectCard from './ProjectCard';
 const projectsData = [
     {
         id:1,
+        title:"Portfolio App Using Next.js",
+        description:"The portfolio site uses Next.js for server-side rendering and routing, Tailwind CSS for utility-first styling, Material UI (MUI) for ready-made React UI components, and Resend for email functionality. These technologies combine to deliver a responsive, performant, and interactive web experience.",
+        image:"/images/projects/portfolio.png",
+        demo:"portofolio-c0pa1jtl8-abdelmageedbahers-projects.vercel.app/",
+        repo:"https://github.com/AbdelmageedBaher/Portofolio.git",
+    },
+    {
+        id:2,
         title:"Ecommerce App Using React.js",
         description:"A modern eCommerce web app built using React.js, Tailwind CSS, and context api, cart management, and responsive design.",
         image:"/images/projects/fresh-cart.png",
@@ -14,7 +22,7 @@ const projectsData = [
         repo:"https://github.com/AbdelmageedBaher/ecommerce-app.git",
     },
     {
-        id:2,
+        id:3,
         title:"Register and Login App ",
         description:"A simple and functional login system built with HTML, CSS, and JavaScript, featuring user authentication and form validation.",
         image:"/images/projects/login.png",
@@ -22,15 +30,16 @@ const projectsData = [
         repo:"https://github.com/AbdelmageedBaher/LoginSystem.git",
     },
     {
-        id:3,
-        title:"Movies App",
-        description:"A dynamic movies web app built with vanilla JavaScript and styled using Tailwind CSS, integrating the TMDB API for real-time movie data.",
-        image:"/images/projects/movie.jpg",
-        demo:"abdelmageedbaher.github.io/movies-app/",
-        repo:"https://github.com/AbdelmageedBaher/ecommerce-app.git",
-    },
-    {
         id:4,
+        title:"Weather App",
+        description:"A simple weather app displaying current conditions by city or location. Built with JavaScript, HTML, CSS and Bootstrap using the OpenWeatherMap API.",
+        image:"/images/projects/weather.jpg",
+        demo:"abdelmageedbaher.github.io/WeatherApp/",
+        repo:"https://github.com/AbdelmageedBaher/WeatherApp.git",
+    },
+    ,
+    {
+        id:5,
         title:"CRUD App",
         description:"A basic CRUD app that stores data locally in the browser using localStorage for persistent and easy data management.",
         image:"/images/projects/crud.png",
@@ -38,7 +47,7 @@ const projectsData = [
         repo:"https://github.com/AbdelmageedBaher/crud.git",
     },
     {
-        id:5,
+        id:6,
         title:"Mealify App",
         description:"A Mealify app built with Bootstrap, HTML, CSS, and JavaScript, featuring a clean, responsive design for browsing meals and recipes.",
         image:"/images/projects/burger.jpg",
@@ -46,13 +55,13 @@ const projectsData = [
         repo:"https://github.com/AbdelmageedBaher/Mealify-Project.git",
     },
     {
-        id:6,
-        title:"Weather App",
-        description:"A simple weather app displaying current conditions by city or location. Built with JavaScript, HTML, CSS and Bootstrap using the OpenWeatherMap API.",
-        image:"/images/projects/weather.jpg",
-        demo:"abdelmageedbaher.github.io/WeatherApp/",
-        repo:"https://github.com/AbdelmageedBaher/WeatherApp.git",
-    },
+        id:7,
+        title:"Movies App",
+        description:"A dynamic movies web app built with vanilla JavaScript and styled using Tailwind CSS, integrating the TMDB API for real-time movie data.",
+        image:"/images/projects/movie.jpg",
+        demo:"abdelmageedbaher.github.io/movies-app/",
+        repo:"https://github.com/AbdelmageedBaher/ecommerce-app.git",
+    }
 ]
 
 export default function Projects() {
