3bde95d9ddd426a4e627824ba7bd92063b0156e0
eng.Abdelmageed <abdelmageedbaher12@gmail.com>
2025-05-30 15:39:18 +0300

    editing

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // next.config.mjs
   images: {
-    domains: ['your-image-domain.com'], // Add the domains you're using here
+    domains: ['i.imgur.com'],
   },
-
 };
-
 
 export default nextConfig;
