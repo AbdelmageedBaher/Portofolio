ee1a1706cd682d7ed319d4a3102621a3fd6f7aff
eng.Abdelmageed <abdelmageedbaher12@gmail.com>
2025-06-08 17:17:59 +0300

    add download cv

--- src/app/component/HeroSection.jsx
+++ src/app/component/HeroSection.jsx
@@ -50,7 +50,7 @@ export default function HeroSection() {
         <div>
             <button className='px-6 py-3 rounded-full w-full sm:w-fit bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-200 mr-4 cursor-pointer text-white'>Hire Me</button>
             <Link
-  href="https://drive.google.com/file/d/1KK-ylfyriXb5hyaR8B_iQ82FeduJTJX4/view?usp=sharing"
+  href="https://drive.google.com/file/d/1Py5IOaWTMAkw9Z2BceaoePSjIoV1hmN6/view?usp=sharing"
   download
   target="_blank"
   rel="noopener noreferrer"
